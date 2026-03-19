ba06592863b9b5e0163bbe392951b97e5154d9ac
Andy <andy@tuxclaw.com>
2026-03-19 12:48:52 -0700

    Fix Supabase URL in __env.js to active project

--- public/__env.js
+++ public/__env.js
@@ -1,4 +1,4 @@
 // This file is used to expose environment variables to the browser in Cloudflare Pages
 window.env = window.env || {};
-window.env.VITE_SUPABASE_URL = "https://qbuhadgmvrwquufqhmrz.supabase.co";
+window.env.VITE_SUPABASE_URL = "https://mbazrezahuojknfgcwou.supabase.co";
 // The anon key will be injected during the build process from Cloudflare environment variables
